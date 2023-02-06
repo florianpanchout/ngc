8f0282957bb12b5af00dac643a6c3555332680a7
Florian <hi@florianpanchout.com>
2023-02-06 19:14:07 +0100

    feat: log all env

--- .github/actions/postComment.js
+++ .github/actions/postComment.js
@@ -3,6 +3,7 @@ const { Octokit } = require('@octokit/action')
 const octokit = new Octokit()
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
 
+console.log(process.env)
 // See https://developer.github.com/v3/issues/#create-an-issue
 octokit
 	.request('POST /repos/{owner}/{repo}/issues', {
