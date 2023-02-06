2ce84b55e3e5ad87c9fa2ba12ee1428290a50d67
Florian <hi@florianpanchout.com>
2023-02-06 19:01:07 +0100

    fix: remove async / await

--- .github/actions/postComment.js
+++ .github/actions/postComment.js
@@ -4,9 +4,10 @@ const octokit = new Octokit()
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
 
 // See https://developer.github.com/v3/issues/#create-an-issue
-const { data } = await octokit.request('POST /repos/{owner}/{repo}/issues', {
-	owner,
-	repo,
-	title: 'My test issue',
-})
-console.log('Issue created: %s', data.html_url)
+octokit
+	.request('POST /repos/{owner}/{repo}/issues', {
+		owner,
+		repo,
+		title: 'My test issue',
+	})
+	.then((res) => console.log(res))
