f82bf07b3e1dabea59c735de54893de0d62e94c4
Florian <hi@florianpanchout.com>
2023-02-13 17:30:24 +0100

    feat: add prod personas

--- tests/testPersonas.js
+++ tests/testPersonas.js
@@ -1,8 +1,9 @@
 /*
-  Test the rules against a set of personas and return the result for each
+  Test the rules against a set of personas and return the result for each, and then compare with production
 
   Command: yarn test
 */
+
 require('isomorphic-fetch')
 
 const Engine = require('publicodes').default
@@ -10,10 +11,10 @@ const Engine = require('publicodes').default
 const rules = require('../public/co2-fr.json')
 const personas = require('../public/personas-fr.json')
 
-const personasRules = Object.values(personas)
-
 const engine = new Engine(rules)
 const missingVariables = Object.keys(engine.evaluate('bilan').missingVariables)
+
+const personasRules = Object.values(personas)
 
 const results = {}
 
@@ -34,41 +35,40 @@ for (persona of personasRules) {
 fetch('https://data.nosgestesclimat.fr/co2-model.FR-lang.fr.json')
 	.then((res) => res.json())
 	.then((prodRules) => {
-		prodEngine = new Engine(prodRules)
-		const prodMissingVariables = Object.keys(
-			prodEngine.evaluate('bilan').missingVariables
-		)
+		fetch('https://data.nosgestesclimat.fr/personas-fr.json')
+			.then((res) => res.json())
+			.then((prodPersonas) => {
+				prodEngine = new Engine(prodRules)
+				const prodMissingVariables = Object.keys(
+					prodEngine.evaluate('bilan').missingVariables
+				)
 
-		const prodResults = {}
+				const prodPersonasRules = Object.values(prodPersonas)
 
-		for (persona of personasRules) {
-			const personaData = persona.data.situation || persona.data
-			const validPersonaRules = Object.keys(personaData).filter((rule) =>
-				prodMissingVariables.includes(rule)
-			)
+				const prodResults = {}
 
-			const validPersonaRulesObject = validPersonaRules.reduce(
-				(acc, cur) => ({ ...acc, [cur]: personaData[cur] }),
-				{}
-			)
-			prodEngine.setSituation(validPersonaRulesObject)
-			prodResults[persona.nom] = prodEngine.evaluate('bilan').nodeValue
-		}
-		console.log('| Nom | Total (PR) | Total (Prod) |')
-		console.log('|:-----|:------:|:------:|')
-		for (let name in results) {
-			const different = results[name] !== prodResults[name]
-			console.log(
-				`|${different ? '⚠️' : '✅'} ${name}|${results[name]}|${
-					prodResults[name]
-				}|`
-			)
-		}
+				for (persona of prodPersonasRules) {
+					const personaData = persona.data.situation || persona.data
+					const validPersonaRules = Object.keys(personaData).filter((rule) =>
+						prodMissingVariables.includes(rule)
+					)
+
+					const validPersonaRulesObject = validPersonaRules.reduce(
+						(acc, cur) => ({ ...acc, [cur]: personaData[cur] }),
+						{}
+					)
+					prodEngine.setSituation(validPersonaRulesObject)
+					prodResults[persona.nom] = prodEngine.evaluate('bilan').nodeValue
+				}
+				console.log('| Nom | Total (PR) | Total (Prod) |')
+				console.log('|:-----|:------:|:------:|')
+				for (let name in results) {
+					const different = results[name] !== prodResults[name]
+					console.log(
+						`|${different ? '⚠️' : '✅'} ${name}|${results[name]}|${
+							prodResults[name]
+						}|`
+					)
+				}
+			})
 	})
-/*
-console.log('| Nom | Total |')
-console.log('|:-----|:------:|')
-for (let name in results) {
-	console.log(`|${name}|${results[name]}|`)
-}
-*/
