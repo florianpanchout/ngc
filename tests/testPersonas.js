ab8c6ba77ed31a0c3c1591e7477ebe3f41bca736
Florian <hi@florianpanchout.com>
2023-02-13 15:40:54 +0100

    feat: add production comparaison

--- tests/testPersonas.js
+++ tests/testPersonas.js
@@ -9,9 +9,9 @@ const Engine = require('publicodes').default
 const rules = require('../public/co2-fr.json')
 const personas = require('../public/personas-fr.json')
 
-const engine = new Engine(rules)
-
 const personasRules = Object.values(personas)
+
+const engine = new Engine(rules)
 const missingVariables = Object.keys(engine.evaluate('bilan').missingVariables)
 
 const results = {}
@@ -26,14 +26,47 @@ for (persona of personasRules) {
 		(acc, cur) => ({ ...acc, [cur]: personaData[cur] }),
 		{}
 	)
-	//console.log(personaData)
-	//console.log(validPersonaRulesObject)
 	engine.setSituation(validPersonaRulesObject)
 	results[persona.nom] = engine.evaluate('bilan').nodeValue
 }
 
+fetch('https://data.nosgestesclimat.fr/co2-fr.json')
+	.then((res) => res.json())
+	.then((prodRules) => {
+		prodEngine = new Engine(prodRules)
+		const prodMissingVariables = Object.keys(
+			prodEngine.evaluate('bilan').missingVariables
+		)
+
+		const prodResults = {}
+
+		for (persona of personasRules) {
+			const personaData = persona.data.situation || persona.data
+			const validPersonaRules = Object.keys(personaData).filter((rule) =>
+				prodMissingVariables.includes(rule)
+			)
+
+			const validPersonaRulesObject = validPersonaRules.reduce(
+				(acc, cur) => ({ ...acc, [cur]: personaData[cur] }),
+				{}
+			)
+			prodEngine.setSituation(validPersonaRulesObject)
+			prodResults[persona.nom] = prodEngine.evaluate('bilan').nodeValue
+		}
+		console.log('yolo 2')
+		console.log('| Nom | Total (PR) | Total (Prod) |')
+		console.log('|:-----|:------:|:------:|')
+		for (let name in results) {
+			const warning = results[name] !== prodResults[name] ? '**' : ''
+			console.log(
+				`|${warning}${name}${warning}|${warning}${results[name]}${warning}|${warning}${prodResults[name]}${warning}|`
+			)
+		}
+	})
+/*
 console.log('| Nom | Total |')
 console.log('|:-----|:------:|')
 for (let name in results) {
 	console.log(`|${name}|${results[name]}|`)
 }
+*/
